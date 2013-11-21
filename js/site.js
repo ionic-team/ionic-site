c21ef6a23491d15795a158d9390236ce527ebf5b
Adam Bradley <adambradley25@gmail.com>
2013-11-20 22:26:36 -0600

    redo exec pattern of site.js

--- js/site.js
+++ js/site.js
@@ -10,6 +10,12 @@
 */
 
 var ionicSite = (function(){
+  var smoothScrollingTo,
+      fixedMenu,
+      winHeight = $(window).height(),
+      docContent = $('.main-content'),
+      devicePreview,
+      defaultScreen;
 
   /* Header menu toggle for mobile */
   $("#menu-toggle").click(function(e) {
@@ -17,26 +23,23 @@ var ionicSite = (function(){
       $(this).toggleClass("active");
   });
 
-  // Smooth Scrolling
-  var smoothScrollingTo;
-  $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          smoothScrollingTo = '#' + target.attr('id');
-          $('html,body').animate({ scrollTop: target.offset().top }, 100, 'swing',
-            function() {
-              if(ionicSite.docContent) {
-                ionicSite.previewSection(smoothScrollingTo);
-              }
-              smoothScrollingTo = undefined;
-            });
-          return false;
-        }
+  // smooth scroll
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        smoothScrollingTo = '#' + target.attr('id');
+        $('html,body').animate({ scrollTop: target.offset().top }, 100, 'swing',
+          function() {
+            if(docContent) {
+              previewSection(smoothScrollingTo);
+            }
+            smoothScrollingTo = undefined;
+          });
+        return false;
       }
-    });
+    }
   });
 
   // left menu link highlight
@@ -45,227 +48,220 @@ var ionicSite = (function(){
     .closest('li')
     .addClass("active");
 
-  return {
 
-    fixedMenu: null,
-    //preFooterTop: $('.pre-footer').offset().top,
-    winHeight:  $(window).height(),
+  /* Fixed left menu */
+  (function() {
+    var activeId;
+    fixedMenu = $('.docked-menu');
+    if(fixedMenu.length) {
 
-    initDockedMenu: function() {
-      /* Fixed left menu */
-      var activeId;
-      ionicSite.fixedMenu = $('.docked-menu');
-      if(ionicSite.fixedMenu.length) {
-
-        var targets = ionicSite.fixedMenu.find('.active-menu').find('a');
-        targets.each(function() {
-          var href = $(this).attr('href');
-          if(href && href.indexOf('#') > -1) {
-            href = href.split('#');
-            href = "#" + href[ href.length - 1 ];
-            $(this).attr('href', href);
-          }
-        });
-
-        var scrollSpyOffset = 40;
-        if( $(document.body).hasClass("device-preview-page") ) {
-          scrollSpyOffset = 300;
+      var targets = fixedMenu.find('.active-menu').find('a');
+      targets.each(function() {
+        var href = $(this).attr('href');
+        if(href && href.indexOf('#') > -1) {
+          href = href.split('#');
+          href = "#" + href[ href.length - 1 ];
+          $(this).attr('href', href);
         }
+      });
 
-        $(document.body).scrollspy({ target: '.docked-menu', offset: scrollSpyOffset });
+      var scrollSpyOffset = 40;
+      if( $(document.body).hasClass("device-preview-page") ) {
+        scrollSpyOffset = 300;
+      }
 
-        var fixedMenuTop = ionicSite.fixedMenu.offset().top;
-        var menuTopPadding = 20;
-        ionicSite.fixedMenu.css({
-          top: menuTopPadding + 'px'
-        });
+      $(document.body).scrollspy({ target: '.docked-menu', offset: scrollSpyOffset });
 
-        function docScroll() {
-          var win = $(window);
-          var scrollTop = win.scrollTop();
-          var winWidth = win.width();
-          if(scrollTop + menuTopPadding > fixedMenuTop && winWidth >= 768) {
-            var bottomView = scrollTop + ionicSite.winHeight;
-            if(false && bottomView > ionicSite.preFooterTop) {
-              // bottom of the page
-              if(!ionicSite.fixedMenu.hasClass("relative-menu")) {
-                var menuTop = ionicSite.fixedMenu.offset().top;
-                ionicSite.fixedMenu
-                  .css({
-                    width: ionicSite.fixedMenu.width() + 'px',
-                    top: menuTop + 'px'
-                  })
-                  .removeClass("fixed-menu")
-                  .addClass("relative-menu");
-              }
-            } else {
-              // middle of the page
-              if(!ionicSite.fixedMenu.hasClass("fixed-menu")) {
-                ionicSite.fixedMenu
-                  .css({
-                    width: ionicSite.fixedMenu.width() + 'px',
-                    top: '20px'
-                  })
-                  .removeClass("relative-menu")
-                  .addClass("fixed-menu");
-              } 
+      var fixedMenuTop = fixedMenu.offset().top;
+      var menuTopPadding = 20;
+      fixedMenu.css({
+        top: menuTopPadding + 'px'
+      });
+
+      function docScroll() {
+        var win = $(window);
+        var scrollTop = win.scrollTop();
+        var winWidth = win.width();
+        if(scrollTop + menuTopPadding > fixedMenuTop && winWidth >= 768) {
+          var bottomView = scrollTop + winHeight;
+          if(false && bottomView > preFooterTop) {
+            // bottom of the page
+            if(!fixedMenu.hasClass("relative-menu")) {
+              var menuTop = fixedMenu.offset().top;
+              fixedMenu
+                .css({
+                  width: fixedMenu.width() + 'px',
+                  top: menuTop + 'px'
+                })
+                .removeClass("fixed-menu")
+                .addClass("relative-menu");
             }
           } else {
-            // top of page
-            if(ionicSite.fixedMenu.hasClass("fixed-menu") || ionicSite.fixedMenu.hasClass("relative-menu")) {
-              ionicSite.fixedMenu
-                .removeClass("relative-menu")
-                .removeClass("fixed-menu")
+            // middle of the page
+            if(!fixedMenu.hasClass("fixed-menu")) {
+              fixedMenu
                 .css({
-                  width: 'auto',
+                  width: fixedMenu.width() + 'px',
                   top: '20px'
-                });
-            }
-            if(scrollTop < 200) {
-              $('.active').removeClass(".active");
-            }
+                })
+                .removeClass("relative-menu")
+                .addClass("fixed-menu");
+            } 
           }
-        }
-        $(window).resize(function() {
-          //ionicSite.preFooterTop = $('.pre-footer').offset().top;
-          ionicSite.winHeight = $(window).height();
-          ionicSite.fixedMenu
+        } else {
+          // top of page
+          if(fixedMenu.hasClass("fixed-menu") || fixedMenu.hasClass("relative-menu")) {
+            fixedMenu
+              .removeClass("relative-menu")
               .removeClass("fixed-menu")
               .css({
-                width: 'auto'
+                width: 'auto',
+                top: '20px'
               });
-          docScroll();
-        });
-        $(window).scroll(docScroll);
+          }
+          if(scrollTop < 200) {
+            $('.active').removeClass(".active");
+          }
+        }
+      }
+      $(window).resize(function() {
+        //preFooterTop = $('.pre-footer').offset().top;
+        winHeight = $(window).height();
+        fixedMenu
+            .removeClass("fixed-menu")
+            .css({
+              width: 'auto'
+            });
+        docScroll();
+      });
+      $(window).scroll(docScroll);
 
-        function scrollSpyChange(e) {
-          if(smoothScrollingTo || !ionicSite.docContent) return;
+      function scrollSpyChange(e) {
+        if(smoothScrollingTo || !docContent) return;
 
-          var id;
-          if(e.target.children.length > 1) {
-            // this is a top level nav link
-            var activeSublinks = $(e.target).find('.active');
-            if(!activeSublinks.length) {
-              // no children are active for this top level link
-              id = e.target.children[0].hash;
-            }
-          } else if(e.target.children.length === 1) {
-            // this is a sub nav link
+        var id;
+        if(e.target.children.length > 1) {
+          // this is a top level nav link
+          var activeSublinks = $(e.target).find('.active');
+          if(!activeSublinks.length) {
+            // no children are active for this top level link
             id = e.target.children[0].hash;
           }
-
-          if(id) {
-            if(ionicSite.devicePreview) {
-              setTimeout(function(){
-                ionicSite.previewSection(id);
-              });
-            } else {
-              var activeSection = $(id);
-              if(activeSection.length) {
-                ionicSite.docContent.find('.active').removeClass('active');
-                activeSection.addClass("active");
-              }
-            }
-          }
+        } else if(e.target.children.length === 1) {
+          // this is a sub nav link
+          id = e.target.children[0].hash;
         }
-        ionicSite.fixedMenu.on('activate.bs.scrollspy', scrollSpyChange);
-      }
-    },
 
-    docContent: $('.main-content'),
-    devicePreview: null,
-
-    initDevicePreview: function() {
-      /* Fixed device preview on the docs page */
-      ionicSite.devicePreview = $('.device-preview');
-      if(ionicSite.devicePreview.length) {
-        var orgDeviceTop = ionicSite.devicePreview.offset().top;
-
-        function onScroll() {
-          if($(window).scrollTop() > orgDeviceTop) {
-            if( !ionicSite.devicePreview.hasClass('fixed-preview') ) {
-              ionicSite.devicePreview
-                .css({
-                  left: Math.round(ionicSite.devicePreview.offset().left) + 'px'
-                })
-                .addClass("fixed-preview");
-              }
+        if(id) {
+          if(devicePreview) {
+            setTimeout(function(){
+              previewSection(id);
+            });
           } else {
-            if( ionicSite.devicePreview.hasClass('fixed-preview') ) {
-              ionicSite.devicePreview
-                .removeClass("fixed-preview")
-                .css({
-                  left: 'auto'
-                });
+            var activeSection = $(id);
+            if(activeSection.length) {
+              docContent.find('.active').removeClass('active');
+              activeSection.addClass("active");
             }
           }
-
         }
-        $(window).resize(function(){
-          ionicSite.devicePreview
+      }
+      fixedMenu.on('activate.bs.scrollspy', scrollSpyChange);
+    }
+  })();
+
+  // initDevicePreview
+  (function() {
+    /* Fixed device preview on the docs page */
+    devicePreview = $('.device-preview');
+    if(devicePreview.length) {
+      var orgDeviceTop = devicePreview.offset().top;
+
+      function onScroll() {
+        if($(window).scrollTop() > orgDeviceTop) {
+          if( !devicePreview.hasClass('fixed-preview') ) {
+            devicePreview
+              .css({
+                left: Math.round(devicePreview.offset().left) + 'px'
+              })
+              .addClass("fixed-preview");
+            }
+        } else {
+          if( devicePreview.hasClass('fixed-preview') ) {
+            devicePreview
               .removeClass("fixed-preview")
               .css({
                 left: 'auto'
               });
-          onScroll();
-        });
-        $(window).scroll(onScroll);
+          }
+        }
+
+      }
+      $(window).resize(function(){
+        devicePreview
+            .removeClass("fixed-preview")
+            .css({
+              left: 'auto'
+            });
         onScroll();
+      });
+      $(window).scroll(onScroll);
+      onScroll();
 
-        var firstSection = ionicSite.docContent.find('.docs-section').first();
-        if(firstSection.length) {
-          ionicSite.previewSection( '#' + firstSection[0].id, true );
-        }
-
-      }
-    },
-
-    previewSection: function(id, doNotSetHistory) {
-      var activeSection = $(id);
-      if(!activeSection.length || !ionicSite.devicePreview) return;
-
-      var title = activeSection.find('h1,h2,h3').first();
-      var newTitle = "Ionic Components";
-      activeId = activeSection.attr('id');
-      if(title.length) {
-        newTitle = title.text() + " - " + newTitle;
-      }
-      document.title = newTitle;
-
-      ionicSite.docContent.find('.active').removeClass('active');
-      activeSection.addClass("active");
-
-      if(!doNotSetHistory && window.history.replaceState) {
-        window.history.replaceState({}, newTitle, "#" + activeId);
+      var firstSection = docContent.find('.docs-section').first();
+      if(firstSection.length) {
+        previewSection( '#' + firstSection[0].id, true );
       }
 
-      ionicSite.devicePreview.find('.active-preview').removeClass('active-preview');
-      var docExample = activeSection.find('.doc-example');
-      if( docExample.length ) {
-        // this 
-        var exampleId = 'example-' + activeId;
-        var examplePreview = $('#' + exampleId);
-        if(examplePreview.length) {
-          // preview has already been added
-          examplePreview.addClass('active-preview');
-        } else if(ionicSite.devicePreview) {
-          // create a new example preview
-          ionicSite.devicePreview.append( '<div id="' + exampleId + '" class="ionic-body">' + docExample.html() + '</div>' );
-          setTimeout(function(){
-            $('#' + exampleId)
-              .addClass('active-preview')
-              .find('a').click(function(){
-                return false;
-              });
-          });
-        }
+    }
+  })();
 
-      } else {
-        if(!ionicSite.defaultScreen) {
-          ionicSite.defaultScreen = ionicSite.devicePreview.find('.default-screen');
-        }
-        ionicSite.defaultScreen.addClass('active-preview');
+
+  function previewSection(id, doNotSetHistory) {
+    var activeSection = $(id);
+    if(!activeSection.length || !devicePreview) return;
+
+    var title = activeSection.find('h1,h2,h3').first();
+    var newTitle = "Ionic Components";
+    activeId = activeSection.attr('id');
+    if(title.length) {
+      newTitle = title.text() + " - " + newTitle;
+    }
+    document.title = newTitle;
+
+    docContent.find('.active').removeClass('active');
+    activeSection.addClass("active");
+
+    if(!doNotSetHistory && window.history.replaceState) {
+      window.history.replaceState({}, newTitle, "#" + activeId);
+    }
+
+    devicePreview.find('.active-preview').removeClass('active-preview');
+    var docExample = activeSection.find('.doc-example');
+    if( docExample.length ) {
+      // this 
+      var exampleId = 'example-' + activeId;
+      var examplePreview = $('#' + exampleId);
+      if(examplePreview.length) {
+        // preview has already been added
+        examplePreview.addClass('active-preview');
+      } else if(devicePreview) {
+        // create a new example preview
+        devicePreview.append( '<div id="' + exampleId + '" class="ionic-body">' + docExample.html() + '</div>' );
+        setTimeout(function(){
+          $('#' + exampleId)
+            .addClass('active-preview')
+            .find('a').click(function(){
+              return false;
+            });
+        });
       }
+
+    } else {
+      if(!defaultScreen) {
+        defaultScreen = devicePreview.find('.default-screen');
+      }
+      defaultScreen.addClass('active-preview');
     }
   }
 

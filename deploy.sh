98504a9c1b1efab47736cf0ef76487de6f90ded1
Andrew <andytjoslin@gmail.com>
2014-08-06 12:15:19 -0600

    chore(): update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,19 @@
-// note, the first time you run this on your computer, you'll need to run this a second time
+#!/bin/bash
 
-// steps:
-// make a change to the site under the master branch
-// commit and push that change
-// fetch all, making sure that you're tracking gh-pages
-// run this script
+# note, the first time you run this on your computer, you'll need to run this a second time
 
+# steps:
+# make a change to the site under the master branch
+# commit and push that change
+# fetch all, making sure that you're tracking gh-pages
+# run this script
+
+MASTER_DIR=$PWD
+PAGES_DIR=$PWD/_site
 
 git branch -D gh-pages
-rm -rf _site
-git clone . _site
+rm -rf $PAGES_DIR
+git clone . $PAGES_DIR
 cd _site
 git checkout --orphan gh-pages
 rm -rf *
@@ -17,10 +21,10 @@ git add --all
 git commit -m "prep for deploy"
 git remote rm origin
 git remote add origin git@github.com:driftyco/ionic-site.git
-cd ..
+cd $MASTER_DIR
 gulp sass
 jekyll build
-cd _site
+cd $PAGES_DIR
 git add --all
 git commit -m "Content creation"
 git push -f origin gh-pages

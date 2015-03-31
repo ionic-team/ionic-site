f91692c693ab10d4baa581609b488c347769c3a3
Max Lynch <max@drifty.com>
2015-03-31 12:42:35 -0500

    Deploy

--- _site/app.js
+++ _site/app.js
@@ -1,18 +1,25 @@
-var express = require("express");
-var app = express();
+var express = require("express"),
+  app = express(),
+  url = require('url');
 
 process.env.PWD = process.cwd()
 
 console.log('PWD', process.env.PWD);
 
-app.use(express.static(process.env.PWD + '/_site'));
-
-// Set up a URL route
 /*
-app.get("/", function(req, res) {
- res.send("Heroku Demo!");
+app.use(function(req,res,next) {
+  var parts = url.parse(req.url);
+
+  if(parts.path.indexOf('/blog/') == 0) {
+    res.redirect(301, 'http://blog.ionic.io/' + req.url.replace(/^\/blog\//, ''));
+  } else {
+    next();
+  }
 });
 */
+
+app.use(express.static(process.env.PWD + '/_site'));
+
 
 // bind the app to listen for connections on a specified port
 var port = process.env.PORT || 3000;

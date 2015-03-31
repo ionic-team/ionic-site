bda764275614278bd910ec64b69ff8fc09454662
Max Lynch <max@drifty.com>
2015-03-31 12:57:17 -0500

    deploy:M  _site/deploy.sh M  deploy.sh

--- _site/deploy.sh
+++ _site/deploy.sh
@@ -2,5 +2,5 @@
 jekyll build
 git add _site/
 msg=$(git status --porcelain | head -c 80)
-git commit -m "deploy: $msg"
+git commit -m "deploy:$msg"
 git push origin master
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 jekyll build
+git add _site/
+msg=$(git status --porcelain | head -c 80)
+git commit -m "deploy:$msg"
 git push origin master

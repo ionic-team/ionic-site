dddc9d0e188082782b0e25892ea0d8099857d40e
perry <perry@drifty.com>
2015-09-24 17:01:19 -0500

    starting v2 scss and related gulp tasks

--- _site/Gulpfile.js
+++ _site/Gulpfile.js
@@ -1,4 +1,5 @@
 var gulp        = require('gulp');
+var $           = require('gulp-load-plugins')();
 var concat      = require('gulp-concat');
 var sass        = require('gulp-sass');
 var minifyCss   = require('gulp-minify-css');
@@ -6,6 +7,7 @@ var rename      = require('gulp-rename');
 var browserSync = require('browser-sync');
 var prefix      = require('gulp-autoprefixer');
 var cp          = require('child_process');
+var pagespeed   = require('psi');
 
 var messages = {
     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
@@ -14,6 +16,56 @@ var messages = {
 var paths = {
   sass: ['scss/**/*.scss']
 };
+
+var AUTOPREFIXER_BROWSERS = [
+  'ie >= 9',
+  'ie_mob >= 9',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
+];
+
+gulp.task('sass', function(done) {
+  return gulp.src('./scss/site.scss')
+    .pipe(sass({onError: browserSync.notify}))
+    .pipe(gulp.dest('./css/'))
+    .pipe(gulp.dest('./_site/css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./css/'))
+    .pipe(gulp.dest('./_site/css/'));
+});
+
+gulp.task('styles:v2', function() {
+  // For best performance, don't add Sass partials to `gulp.src`
+  return gulp.src('_scss/v2.scss')
+    .pipe($.sourcemaps.init())
+    .pipe(sass({
+      precision: 10,
+      onError: console.error.bind(console, 'Sass error:')
+    }))
+    .pipe($.autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+    //
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('./css/'))
+    .pipe(gulp.dest('./_site/css/'))
+
+    // .pipe($.sourcemaps.write('./css/'))
+    // Concatenate and minify styles
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./css/'))
+    .pipe(gulp.dest('./_site/css/'))
+    .pipe($.size({title: 'styles'}));
+});
 
 gulp.task('sass', function(done) {
   return gulp.src('./scss/site.scss')
@@ -59,12 +111,16 @@ gulp.task('server', ['sass', 'jekyll-build'], function() {
 gulp.task('server:styles', ['sass'], function() {
   browserSync.reload();
 });
+gulp.task('server:stylesv2', ['styles:v2'], function() {
+  browserSync.reload();
+});
 gulp.task('server:jekyll', ['jekyll-build'], function() {
   browserSync.reload();
 });
 
 gulp.task('watch', ['server'],function() {
   gulp.watch('scss/**.scss', ['server:styles']);
+  gulp.watch(['_scss/*.scss', '_scss/docs/*.scss'], ['server:stylesv2']);
   gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['server:jekyll']);
 
 });

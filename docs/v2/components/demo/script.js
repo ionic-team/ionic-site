681aedc43636be66fadf8fbbccdad02e57d22d03
Drew Rygh <drew.rygh@gmail.com>
2015-09-30 11:13:45 -0500

    docs: change iframe demo when message is received

--- docs/v2/components/demo/script.js
+++ docs/v2/components/demo/script.js
@@ -2,9 +2,19 @@ angular.module('ionicApp', ['ionic'])
 
 .controller('AppCtrl', function ($scope, $ionicActionSheet) {
 
+  function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  }
+
   $scope.component = {
     title: 'Action Sheet',
   }
+
+  window.onmessage = function(e){
+    $scope.$apply(function() {
+      $scope.component.title = toTitleCase(e.data.replace('-', ' '));
+    });
+  };
 
   $scope.showActionsheet = function () {
 

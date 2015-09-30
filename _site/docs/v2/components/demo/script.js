8de959c0e5c8d5512ab019e39fe6a482ef91c9da
perry <perry@drifty.com>
2015-09-30 12:37:48 -0500

    new build

--- _site/docs/v2/components/demo/script.js
+++ _site/docs/v2/components/demo/script.js
@@ -2,6 +2,20 @@ angular.module('ionicApp', ['ionic'])
 
 .controller('AppCtrl', function ($scope, $ionicActionSheet) {
 
+  function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  }
+
+  $scope.component = {
+    title: 'Action Sheet',
+  }
+
+  window.onmessage = function(e){
+    $scope.$apply(function() {
+      $scope.component.title = toTitleCase(e.data.replace('-', ' '));
+    });
+  };
+
   $scope.showActionsheet = function () {
 
     $ionicActionSheet.show({

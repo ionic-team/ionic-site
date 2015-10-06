d85863f4d4d5726c664bd71b51132472eaec1bf2
Drew Rygh <drew.rygh@gmail.com>
2015-10-06 10:44:43 -0500

    docs(components) modal demo outline

--- docs/v2/components/demo/index.js
+++ docs/v2/components/demo/index.js
@@ -1,15 +1,15 @@
-System.register("index", ["ionic/ionic", "angular2/angular2", "navigation", "tabs", "helpers"], function (_export) {
+System.register("index", ["ionic/ionic", "angular2/angular2", "navigation", "tabs", "modal", "helpers"], function (_export) {
     "use strict";
 
-    var App, IonicApp, ActionSheet, NavController, NavParams, Modal, IonicView, Events, NgZone, NavigationDetailsPage, TabsPage, helpers, __decorate, __metadata, DemoModal, MainPage, DemoApp, _a, _b, _c, _d, _e, _f, _g;
-
-    var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+    var App, IonicApp, ActionSheet, NavController, NavParams, Modal, IonicView, Events, Animation, NgZone, NavigationDetailsPage, TabsPage, DemoModal, helpers, __decorate, __metadata, MainPage, FadeIn, FadeOut, DemoApp, _a, _b, _c, _d, _e, _f, _g;
 
     var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
-    function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+    var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
     function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+    function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
     return {
         setters: [function (_ionicIonic) {
@@ -21,12 +21,15 @@ System.register("index", ["ionic/ionic", "angular2/angular2", "navigation", "tab
             Modal = _ionicIonic.Modal;
             IonicView = _ionicIonic.IonicView;
             Events = _ionicIonic.Events;
+            Animation = _ionicIonic.Animation;
         }, function (_angular2Angular2) {
             NgZone = _angular2Angular2.NgZone;
         }, function (_navigation) {
             NavigationDetailsPage = _navigation.NavigationDetailsPage;
         }, function (_tabs) {
             TabsPage = _tabs.TabsPage;
+        }, function (_modal) {
+            DemoModal = _modal.DemoModal;
         }, function (_helpers) {
             helpers = _helpers;
         }],
@@ -53,22 +56,6 @@ System.register("index", ["ionic/ionic", "angular2/angular2", "navigation", "tab
                 if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
             };
 
-            DemoModal = (function (_Modal) {
-                _inherits(DemoModal, _Modal);
-
-                function DemoModal() {
-                    _classCallCheck(this, DemoModal);
-
-                    _get(Object.getPrototypeOf(DemoModal.prototype), "constructor", this).call(this);
-                }
-
-                return DemoModal;
-            })(Modal);
-
-            DemoModal = __decorate([IonicView({
-                template: '<div>My Modal</div>'
-            }), __metadata('design:paramtypes', [])], DemoModal);
-
             MainPage = (function () {
                 function MainPage(app, nav, actionSheet, zone, params, modal, events) {
                     var _this = this;
@@ -81,6 +68,7 @@ System.register("index", ["ionic/ionic", "angular2/angular2", "navigation", "tab
                     this.modal = modal;
                     this.actionSheet = actionSheet;
                     this.navDetailsPage = NavigationDetailsPage;
+                    this.demoModal = DemoModal;
                     if (params.data.location) {
                         this.component.title = params.data.location;
                     } else if (window.location.hash) {
@@ -149,10 +137,10 @@ System.register("index", ["ionic/ionic", "angular2/angular2", "navigation", "tab
                 }, {
                     key: "openModal",
                     value: function openModal() {
-                        this.modal.open(DemoModal, {
+                        this.modal.open(this.demoModal, {
+                            handle: 'my-awesome-modal',
                             enterAnimation: 'my-fade-in',
-                            leaveAnimation: 'my-fade-out',
-                            handle: 'my-awesome-modal'
+                            leaveAnimation: 'my-fade-out'
                         });
                     }
                 }]);
@@ -165,6 +153,36 @@ System.register("index", ["ionic/ionic", "angular2/angular2", "navigation", "tab
             _export("MainPage", MainPage = __decorate([IonicView({
                 templateUrl: 'main.html'
             }), __metadata('design:paramtypes', [typeof (_a = typeof IonicApp !== 'undefined' && IonicApp) === 'function' && _a || Object, typeof (_b = typeof NavController !== 'undefined' && NavController) === 'function' && _b || Object, typeof (_c = typeof ActionSheet !== 'undefined' && ActionSheet) === 'function' && _c || Object, typeof (_d = typeof NgZone !== 'undefined' && NgZone) === 'function' && _d || Object, typeof (_e = typeof NavParams !== 'undefined' && NavParams) === 'function' && _e || Object, typeof (_f = typeof Modal !== 'undefined' && Modal) === 'function' && _f || Object, typeof (_g = typeof Events !== 'undefined' && Events) === 'function' && _g || Object])], MainPage));
+
+            FadeIn = (function (_Animation) {
+                _inherits(FadeIn, _Animation);
+
+                function FadeIn(element) {
+                    _classCallCheck(this, FadeIn);
+
+                    _get(Object.getPrototypeOf(FadeIn.prototype), "constructor", this).call(this, element);
+                    this.easing('ease').duration(450).fadeIn();
+                }
+
+                return FadeIn;
+            })(Animation);
+
+            Animation.register('my-fade-in', FadeIn);
+
+            FadeOut = (function (_Animation2) {
+                _inherits(FadeOut, _Animation2);
+
+                function FadeOut(element) {
+                    _classCallCheck(this, FadeOut);
+
+                    _get(Object.getPrototypeOf(FadeOut.prototype), "constructor", this).call(this, element);
+                    this.easing('ease').duration(250).fadeOut();
+                }
+
+                return FadeOut;
+            })(Animation);
+
+            Animation.register('my-fade-out', FadeOut);
 
             DemoApp = function DemoApp() {
                 _classCallCheck(this, DemoApp);

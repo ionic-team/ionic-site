29b70efb6d57597e78b2012b46e19b499200b04b
Drew Rygh <drew.rygh@gmail.com>
2015-10-08 14:15:12 -0500

    deploy:M  _site/docs/v2/components/demo/forms/forms.js M  _site/docs/v2/components/demo

--- docs/v2/components/demo/forms/forms.js
+++ docs/v2/components/demo/forms/forms.js
@@ -1,20 +1,20 @@
-System.register("forms/forms", ["angular2/forms", "ionic/ionic"], function (_export) {
+System.register("forms/forms", ["angular2/angular2", "ionic/ionic"], function (_export) {
     "use strict";
 
-    var FormBuilder, Validators, Control, ControlGroup, IonicView, __decorate, __metadata, FormsPage;
+    var FormBuilder, Validators, Control, ControlGroup, Page, __decorate, __metadata, FormsPage;
 
     var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
     function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
     return {
-        setters: [function (_angular2Forms) {
-            FormBuilder = _angular2Forms.FormBuilder;
-            Validators = _angular2Forms.Validators;
-            Control = _angular2Forms.Control;
-            ControlGroup = _angular2Forms.ControlGroup;
+        setters: [function (_angular2Angular2) {
+            FormBuilder = _angular2Angular2.FormBuilder;
+            Validators = _angular2Angular2.Validators;
+            Control = _angular2Angular2.Control;
+            ControlGroup = _angular2Angular2.ControlGroup;
         }, function (_ionicIonic) {
-            IonicView = _ionicIonic.IonicView;
+            Page = _ionicIonic.Page;
         }],
         execute: function () {
             __decorate = undefined && undefined.__decorate || function (decorators, target, key, desc) {
@@ -62,7 +62,7 @@ System.register("forms/forms", ["angular2/forms", "ionic/ionic"], function (_exp
 
             _export("FormsPage", FormsPage);
 
-            _export("FormsPage", FormsPage = __decorate([IonicView({
+            _export("FormsPage", FormsPage = __decorate([Page({
                 templateUrl: 'forms/forms.html',
                 bindings: [FormBuilder]
             }), __metadata('design:paramtypes', [])], FormsPage));

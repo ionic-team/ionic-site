e214d0fbbcfa9bf795894e76d58527018fa8243d
Drew Rygh <drew.rygh@gmail.com>
2015-10-19 11:49:36 -0500

    docs(demos): use default animation

--- docs/v2/components/demo/modals/modals.js
+++ docs/v2/components/demo/modals/modals.js
@@ -4,11 +4,7 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
-
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -46,11 +42,7 @@ var ModalsFirstPage = (function () {
     _createClass(ModalsFirstPage, [{
         key: "openModal",
         value: function openModal() {
-            this.modal.open(ModalsContentPage, {
-                handle: 'my-awesome-modal',
-                enterAnimation: 'my-fade-in',
-                leaveAnimation: 'my-fade-out'
-            });
+            this.modal.open(ModalsContentPage);
         }
     }]);
 
@@ -90,34 +82,4 @@ exports.ModalsPage = ModalsPage;
 exports.ModalsPage = ModalsPage = __decorate([(0, _ionicIonic.Page)({
     template: '<ion-nav [root]="rootView"></ion-nav>'
 }), __metadata('design:paramtypes', [])], ModalsPage);
-
-var FadeIn = (function (_Animation) {
-    _inherits(FadeIn, _Animation);
-
-    function FadeIn(element) {
-        _classCallCheck(this, FadeIn);
-
-        _get(Object.getPrototypeOf(FadeIn.prototype), "constructor", this).call(this, element);
-        this.easing('ease').duration(450).fadeIn();
-    }
-
-    return FadeIn;
-})(_ionicIonic.Animation);
-
-_ionicIonic.Animation.register('my-fade-in', FadeIn);
-
-var FadeOut = (function (_Animation2) {
-    _inherits(FadeOut, _Animation2);
-
-    function FadeOut(element) {
-        _classCallCheck(this, FadeOut);
-
-        _get(Object.getPrototypeOf(FadeOut.prototype), "constructor", this).call(this, element);
-        this.easing('ease').duration(250).fadeOut();
-    }
-
-    return FadeOut;
-})(_ionicIonic.Animation);
-
-_ionicIonic.Animation.register('my-fade-out', FadeOut);
 var _a, _b, _c, _d, _e;

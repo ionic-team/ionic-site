5991300dadc46b8b53409298d2702fdd3e6d7690
Max Lynch <ihasmax@gmail.com>
2015-11-11 10:41:36 -0600

    redirect www to non wwww

--- app.js
+++ app.js
@@ -19,6 +19,16 @@ app.use(function(req,res,next) {
 
 });
 
+function wwwRedirect(req, res, next) {
+    if (req.headers.host.slice(0, 4) === 'www.') {
+        var newHost = req.headers.host.slice(4);
+        return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
+    }
+    next();
+};
+app.set('trust proxy', true);
+app.use(wwwRedirect);
+
 app.use(express.static(process.env.PWD + '/_site'));
 
 

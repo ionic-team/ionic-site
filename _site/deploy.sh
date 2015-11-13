b68e2ac631904eff67512c4cc8819bd1f50317bc
perry <perry@ionic.io>
2015-11-13 11:33:15 -0600

    removing _site from master w/ an updated branch

--- _site/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-jekyll build
-git add _site/
-msg=$(git status --porcelain | head -c 80)
-git commit -m "deploy:$msg"
-git push origin master
--- _site/docs/v2/dist/ionic/components/tabbar/mixins/placement/placement.js
+++
@@ -1,8 +0,0 @@
-System.register("ionic/components/tabbar/mixins/placement/placement", [], function($__export) {
-  "use strict";
-  var __moduleName = "ionic/components/tabbar/mixins/placement/placement";
-  return {
-    setters: [],
-    execute: function() {}
-  };
-});

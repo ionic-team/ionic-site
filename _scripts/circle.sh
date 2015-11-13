2461306a24ad6c24fea13648f5aea2a69039c0af
perry <perry@ionic.io>
2015-11-13 12:37:02 -0600

    new circle CI deployments, attempt #1

--- _scripts/circle.sh
+++ _scripts/circle.sh
@@ -6,6 +6,26 @@ ARG_DEFS=(
 )
 
 function run {
-  echo "Hello World"
+  echo "Cloning ionic-site in _site"
+  mkdir ./_site
+  cd _site
+  git clone https://$GH_AUTH_TOKEN@github.com:driftyco/ionic-site.git .
 
+  echo "Checking out production branch"
+  git checkout origin/production -b production
+
+  echo "Building site with jekyll"
+  cd ..
+  jekyll build
+
+  echo "Committing to production branch"
+  cd ./_site
+  git commit -a -m "automated build of $CIRCLE_SHA1"
+
+  echo "Pushing production branch to origin"
+  git push origin production
+  cd ..
+
+  echo "Cleaning up"
+  rm -R _site
 }

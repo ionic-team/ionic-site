ca0843a50e50ee980d639e74960738f7faa0c721
perry <perry@ionic.io>
2015-11-16 17:23:24 -0600

    dont' run CI on production branch

--- _scripts/test.sh
+++ _scripts/test.sh
@@ -8,6 +8,8 @@ ARG_DEFS=(
 function buildforjekyll {
   echo "Building site with jekyll"
   jekyll build
+  # don't run circle on production branch
+  rm _site/circle.yml
 }
 
 function runlinkchecker {

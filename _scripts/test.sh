514ab769331b7aa43cea1582e7522ad2be455b08
perry <perry@ionic.io>
2015-11-16 17:18:26 -0600

    adding linkchecking and outputting limiting tests to master branch and its forks

--- _scripts/test.sh
+++ _scripts/test.sh
@@ -10,6 +10,16 @@ function buildforjekyll {
   jekyll build
 }
 
+function runlinkchecker {
+  echo "Starting linkchecker"
+  cd _site
+  python -m SimpleHTTPServer 8000 &> /dev/null &
+  pid=$!
+  linkchecker http://localhost:8000 -ohtml --timeout=5 --ignore-url=^mailto > linkchecker.html
+  kill $pid
+}
+
 if [ -d "_scripts" ]; then
   buildforjekyll
+  runlinkchecker
 fi

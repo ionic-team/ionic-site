df95795bb257873c4210510bc2f6bd87acbaa67e
ionitron <hi@ionicframework.com>
2015-11-16 12:00:14 -0600

    CI test

--- _scripts/circle.sh
+++ _scripts/circle.sh
@@ -9,7 +9,7 @@ function buildforjekyll {
   echo "Cloning ionic-site in _site"
   mkdir ./_site
   cd _site
-  git clone https://$CIRCLE_SHA1@github.com/driftyco/ionic-site.git .
+  git clone https://$GH_AUTH_TOKEN@github.com/driftyco/ionic-site.git .
   git config --global user.email "hi@ionicframework.com"
   git config --global user.name "ionitron"
   # git init
@@ -24,7 +24,7 @@ function buildforjekyll {
 
   echo "Committing to production branch"
   cd ./_site
-  git add .
+  git add . --all
   git commit -m "automated build of $CIRCLE_SHA1"
 
   echo "Pushing production branch to origin"

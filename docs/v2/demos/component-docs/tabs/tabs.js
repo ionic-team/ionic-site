8276a7809dae6cf932bfdac094f4ab4987d9ac00
Drew Rygh <drew.rygh@gmail.com>
2016-01-08 16:49:09 -0600

    components demo => preview-app

--- docs/v2/demos/component-docs/tabs/tabs.js
+++
@@ -1,6 +0,0 @@
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./basic/pages'));
-__export(require('./icon/pages'));
-__export(require('./icon-text/pages'));

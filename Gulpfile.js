5133884803c0161fa2b742975f448e5afe212ea7
perry <perry@ionic.io>
2016-01-15 16:20:02 -0600

    massively improve build times and fix creator link

--- Gulpfile.js
+++ Gulpfile.js
@@ -109,7 +109,7 @@ gulp.task('js', function() {
  */
 gulp.task('jekyll-build', ['cli-docs'], function(done) {
   browserSync.notify(messages.jekyllBuild);
-  return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+  return cp.spawn('jekyll', ['build', '-I'], {stdio: 'inherit'})
            .on('close', done);
 });
 

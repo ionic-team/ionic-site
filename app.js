40fa40a02e0e1333a6fc0703457e69025bdb5fcb
perry <perry@ionic.io>
2016-01-20 16:37:39 -0600

    adding gzip, expires headers, and minimizing requests in v2 docs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express"),
-  app = express(),
-  url = require('url');
+    app = express(),
+    url = require('url'),
+    compress = require('compression');
 
 process.env.PWD = process.cwd()
 
@@ -27,14 +28,25 @@ app.use(function(req, res, next) {
 });
 
 function wwwRedirect(req, res, next) {
-    if (req.headers.host.slice(0, 4) === 'www.') {
-        var newHost = req.headers.host.slice(4);
-        return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
-    }
-    next();
+  if (req.headers.host.slice(0, 4) === 'www.') {
+    var newHost = req.headers.host.slice(4);
+    return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
+  }
+  next();
 };
 app.set('trust proxy', true);
 app.use(wwwRedirect);
+app.use(compress());
+app.use(function(req, res, next) {
+  var staticURLS = ['/img/','/css/','/js/','/favicon.ico'];
+  for (var i = 0; i < staticURLS.length; i++) {
+    if (req.url.indexOf(staticURLS[i]) === 0) {
+      res.setHeader('Cache-Control', 'public, max-age=345600'); // 4 days
+      res.setHeader('Expires', new Date(Date.now() + 345600000).toUTCString());
+    }
+  }
+  return next();
+});
 
 app.use(express.static(process.env.PWD));
 

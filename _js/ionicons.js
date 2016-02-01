c0ab3b978b4a30c964556e9c3a29fc06c9efbee5
perry <perry@ionic.io>
2016-02-01 15:22:05 -0600

    ionicons tweaks

--- _js/ionicons.js
+++ _js/ionicons.js
@@ -14,16 +14,15 @@ $(document).ready(function() {
       zIndex: 1,              // Overlay z-index.
       escapeClose: true,      // Allows the user to close the modal by pressing `ESC`
       clickClose: true,       // Allows the user to close the modal by clicking the overlay
-      closeText: 'Close',     // Text content for the close <a> tag.
-      closeClass: '',         // Add additional class(es) to the close <a> tag.
+      closeText: 'X',     // Text content for the close <a> tag.
+      closeClass: 'close',         // Add additional class(es) to the close <a> tag.
       showClose: true,        // Shows a (X) icon/link in the top-right corner
-      modalClass: "modal",    // CSS class added to the element being displayed in the modal.
+      modalClass: "shown",    // CSS class added to the element being displayed in the modal.
       spinnerHtml: null,      // HTML appended to the default spinner during AJAX requests.
       showSpinner: true,      // Enable/disable the default spinner during AJAX requests.
-      fadeDuration: null,     // Number of milliseconds the fade transition takes (null means no transition)
+      fadeDuration: 5000,     // Number of milliseconds the fade transition takes (null means no transition)
       fadeDelay: 1.0          // Point during the overlay's fade-in that the modal begins to fade in (.5 = 50%, 1.5 = 150%, etc.)
     };
-
     function loadJSON(callback) {
        var xobj = new XMLHttpRequest();
            xobj.overrideMimeType("application/json");
@@ -279,4 +278,3 @@ $(document).ready(function() {
 
   });
 }
-

b66e254b2168c14e3ebe9803a2c29df8e61bfde7
Ionitron <hi@ionicframework.com>
2016-02-03 03:05:04 +0000

    docs: update for 2.0.0-alpha.54

---
+++ docs/v2/demos/alert/index.js
@@ -0,0 +1,206 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var ionic_1 = require('ionic/ionic');
+var ApiDemoApp = (function () {
+    function ApiDemoApp() {
+        this.rootPage = InitialPage;
+    }
+    ApiDemoApp = __decorate([
+        ionic_1.App({
+            templateUrl: 'app.html'
+        }), 
+        __metadata('design:paramtypes', [])
+    ], ApiDemoApp);
+    return ApiDemoApp;
+})();
+var InitialPage = (function () {
+    function InitialPage(nav) {
+        this.nav = nav;
+    }
+    InitialPage.prototype.doAlert = function () {
+        var alert = ionic_1.Alert.create({
+            title: 'New Friend!',
+            subTitle: 'Your friend, Obi wan Kenobi, just accepted your friend request!',
+            buttons: ['Ok']
+        });
+        this.nav.present(alert);
+    };
+    InitialPage.prototype.doConfirm = function () {
+        var alert = ionic_1.Alert.create({
+            title: 'Use this lightsaber?',
+            body: 'Do you agree to use this lightsaber to do good across the intergalactic galaxy?',
+            buttons: [
+                {
+                    text: 'Disagree',
+                    handler: function () {
+                        console.log('Disagree clicked');
+                    }
+                },
+                {
+                    text: 'Agree',
+                    handler: function () {
+                        console.log('Agree clicked');
+                    }
+                }
+            ]
+        });
+        this.nav.present(alert);
+    };
+    InitialPage.prototype.doPrompt = function () {
+        var alert = ionic_1.Alert.create({
+            title: 'Login',
+            body: "Enter a name for this new album you're so keen on adding",
+            inputs: [
+                {
+                    name: 'title',
+                    placeholder: 'Title'
+                },
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    handler: function (data) {
+                        console.log('Cancel clicked');
+                    }
+                },
+                {
+                    text: 'Save',
+                    handler: function (data) {
+                        console.log('Saved clicked');
+                    }
+                }
+            ]
+        });
+        this.nav.present(alert);
+    };
+    InitialPage.prototype.doRadio = function () {
+        var _this = this;
+        var alert = ionic_1.Alert.create();
+        alert.setTitle('Lightsaber color');
+        alert.addInput({
+            type: 'radio',
+            label: 'Blue',
+            value: 'blue',
+            checked: true
+        });
+        alert.addInput({
+            type: 'radio',
+            label: 'Green',
+            value: 'green'
+        });
+        alert.addInput({
+            type: 'radio',
+            label: 'Red',
+            value: 'red'
+        });
+        alert.addInput({
+            type: 'radio',
+            label: 'Yellow',
+            value: 'yellow'
+        });
+        alert.addInput({
+            type: 'radio',
+            label: 'Purple',
+            value: 'purple'
+        });
+        alert.addInput({
+            type: 'radio',
+            label: 'White',
+            value: 'white'
+        });
+        alert.addInput({
+            type: 'radio',
+            label: 'Black',
+            value: 'black'
+        });
+        alert.addButton('Cancel');
+        alert.addButton({
+            text: 'Ok',
+            handler: function (data) {
+                console.log('Radio data:', data);
+                _this.testRadioOpen = false;
+                _this.testRadioResult = data;
+            }
+        });
+        this.nav.present(alert);
+    };
+    InitialPage.prototype.doCheckbox = function () {
+        var _this = this;
+        var alert = ionic_1.Alert.create();
+        alert.setTitle('Which planets have you visited?');
+        alert.addInput({
+            type: 'checkbox',
+            label: 'Alderaan',
+            value: 'value1',
+            checked: true
+        });
+        alert.addInput({
+            type: 'checkbox',
+            label: 'Bespin',
+            value: 'value2'
+        });
+        alert.addInput({
+            type: 'checkbox',
+            label: 'Coruscant',
+            value: 'value3'
+        });
+        alert.addInput({
+            type: 'checkbox',
+            label: 'Endor',
+            value: 'value4'
+        });
+        alert.addInput({
+            type: 'checkbox',
+            label: 'Hoth',
+            value: 'value5'
+        });
+        alert.addInput({
+            type: 'checkbox',
+            label: 'Jakku',
+            value: 'value6'
+        });
+        alert.addInput({
+            type: 'checkbox',
+            label: 'Naboo',
+            value: 'value6'
+        });
+        alert.addInput({
+            type: 'checkbox',
+            label: 'Takodana',
+            value: 'value6'
+        });
+        alert.addInput({
+            type: 'checkbox',
+            label: 'Tatooine',
+            value: 'value6'
+        });
+        alert.addButton('Cancel');
+        alert.addButton({
+            text: 'Okay',
+            handler: function (data) {
+                console.log('Checkbox data:', data);
+                _this.testCheckboxOpen = false;
+                _this.testCheckboxResult = data;
+            }
+        });
+        this.nav.present(alert);
+    };
+    InitialPage.prototype.doCustomAnimation = function () {
+    };
+    InitialPage = __decorate([
+        ionic_1.Page({
+            templateUrl: 'main.html'
+        }), 
+        __metadata('design:paramtypes', [(typeof (_a = typeof ionic_1.NavController !== 'undefined' && ionic_1.NavController) === 'function' && _a) || Object])
+    ], InitialPage);
+    return InitialPage;
+    var _a;
+})();
+exports.InitialPage = InitialPage;

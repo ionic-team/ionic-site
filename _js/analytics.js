068202b2cd629dfa434030166cbf098419f39368
perry <perry@ionic.io>
2016-02-17 17:35:16 -0600

    analytics - tracking creator clicks and adding ga to v2

--- _js/analytics.js
+++ _js/analytics.js
@@ -12,3 +12,12 @@ var _sf_async_config = { uid: 54141, domain: 'ionicframework.com', useCanonical:
   window.onload = (typeof window.onload != 'function') ?
     loadChartbeat : function() { oldonload(); loadChartbeat(); };
 })();
+
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-44023830-1', 'ionicframework.com');
+ga('send', 'pageview');
--- js/ionic-docs.js
+++ js/ionic-docs.js
@@ -18,6 +18,15 @@ var _sf_async_config = { uid: 54141, domain: 'ionicframework.com', useCanonical:
     loadChartbeat : function() { oldonload(); loadChartbeat(); };
 })();
 
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-44023830-1', 'ionicframework.com');
+ga('send', 'pageview');
+
 var IonicDocsModule = angular.module('IonicDocs', ['ngAnimate'])
 .controller('DocsNavCtrl', ['$scope', '$timeout', function($scope, $timeout) {
   $scope.test = function() {

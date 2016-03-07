3785e218c3fe73487329f6382077c690aa865c4b
Ionitron <hi@ionicframework.com>
2016-03-07 15:34:47 +0000

    docs: update for 2.0.0-beta.2

--- docs/v2/demos/list/bundle.js
+++ docs/v2/demos/list/bundle.js
@@ -27302,11 +27302,14 @@
 	        var startIndex = url.indexOf('?');
 	        if (startIndex !== -1) {
 	            var queries = url.slice(startIndex + 1).split('&');
-	            queries.filter(function (param) { return param.indexOf('=') > 0; }).forEach(function (param) {
-	                var split = param.split('=');
-	                if (split.length > 1)
-	                    queryParams[split[0].toLowerCase()] = split[1].split('#')[0];
-	            });
+	            for (var i = 0; i < queries.length; i++) {
+	                if (queries[i].indexOf('=') > 0) {
+	                    var split = queries[i].split('=');
+	                    if (split.length > 1) {
+	                        queryParams[split[0].toLowerCase()] = split[1].split('#')[0];
+	                    }
+	                }
+	            }
 	        }
 	    }
 	    return queryParams;

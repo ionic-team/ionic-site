84bfc46114eee890800d436ed2e16961a2b5c643
Ionitron <hi@ionicframework.com>
2016-04-15 19:27:51 +0000

    Automated build of ionic  v2.0.0-beta.4 driftyco/ionic@43c6e096af6c6cea6f42bab16aac132d0eef5bd8

--- docs/v2/demos/action-sheet/bundle.js
+++ docs/v2/demos/action-sheet/bundle.js
@@ -62569,19 +62569,19 @@
 	    NativeInput.prototype._focus = function () {
 	        var self = this;
 	        self.focusChange.emit(true);
+	        function docTouchEnd(ev) {
+	            var tappedElement = ev.target;
+	            if (tappedElement && self.element()) {
+	                if (tappedElement.tagName !== "INPUT" && tappedElement.tagName !== "TEXTAREA") {
+	                    self.element().blur();
+	                }
+	            }
+	        }
 	        if (self._blurring) {
 	            // automatically blur input if:
 	            // 1) this input has focus
 	            // 2) the newly tapped document element is not an input
 	            console.debug('input blurring enabled');
-	            function docTouchEnd(ev) {
-	                var tappedElement = ev.target;
-	                if (tappedElement && self.element()) {
-	                    if (tappedElement.tagName !== 'INPUT' && tappedElement.tagName !== 'TEXTAREA') {
-	                        self.element().blur();
-	                    }
-	                }
-	            }
 	            document.addEventListener('touchend', docTouchEnd, true);
 	            self._unrefBlur = function () {
 	                console.debug('input blurring disabled');

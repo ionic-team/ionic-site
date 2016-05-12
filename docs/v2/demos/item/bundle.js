e2af1d355ce4af8f412dd8a109eed5b69c27eac0
Ionitron <hi@ionicframework.com>
2016-05-12 23:03:00 +0000

    Automated build of ionic  v2.0.0-beta.6 driftyco/ionic@71cd29775174f35eb4d50f66d9cd8383e6d67f72

--- docs/v2/demos/item/bundle.js
+++ docs/v2/demos/item/bundle.js
@@ -65333,14 +65333,17 @@
 	     * @private
 	     */
 	    TextInput.prototype.clearTextInput = function () {
+	        console.debug("Should clear input");
 	        this._value = '';
+	        this.onChange(this._value);
+	        this.writeValue(this._value);
 	    };
 	    TextInput = __decorate([
 	        core_1.Component({
 	            selector: 'ion-input',
 	            template: '<input [type]="type" [(ngModel)]="_value" (blur)="inputBlurred($event)" (focus)="inputFocused($event)" [placeholder]="placeholder" class="text-input">' +
 	                '<input [type]="type" aria-hidden="true" next-input *ngIf="_useAssist">' +
-	                '<button clear *ngIf="clearInput && value" type="button" class="text-input-clear-icon" (click)="clearTextInput()" (mousedown)="clearTextInput()"></button>' +
+	                '<button clear [hidden]="!clearInput" type="button" class="text-input-clear-icon" (click)="clearTextInput()" (mousedown)="clearTextInput()"></button>' +
 	                '<div (touchstart)="pointerStart($event)" (touchend)="pointerEnd($event)" (mousedown)="pointerStart($event)" (mouseup)="pointerEnd($event)" class="input-cover" tappable *ngIf="_useAssist"></div>',
 	            directives: [
 	                native_input_1.NextInput,
@@ -65782,13 +65785,6 @@
 	            this.setFocus();
 	            this.regScrollMove();
 	        }
-	    };
-	    /**
-	      * @private
-	     */
-	    InputBase.prototype.clearTextInput = function () {
-	        console.debug('Should clear input');
-	        this._value = '';
 	    };
 	    /**
 	     * @private
--- docs/v2/demos/navigation/bundle.js
+++ docs/v2/demos/navigation/bundle.js
@@ -65376,14 +65376,17 @@
 	     * @private
 	     */
 	    TextInput.prototype.clearTextInput = function () {
+	        console.debug("Should clear input");
 	        this._value = '';
+	        this.onChange(this._value);
+	        this.writeValue(this._value);
 	    };
 	    TextInput = __decorate([
 	        core_1.Component({
 	            selector: 'ion-input',
 	            template: '<input [type]="type" [(ngModel)]="_value" (blur)="inputBlurred($event)" (focus)="inputFocused($event)" [placeholder]="placeholder" class="text-input">' +
 	                '<input [type]="type" aria-hidden="true" next-input *ngIf="_useAssist">' +
-	                '<button clear *ngIf="clearInput && value" type="button" class="text-input-clear-icon" (click)="clearTextInput()" (mousedown)="clearTextInput()"></button>' +
+	                '<button clear [hidden]="!clearInput" type="button" class="text-input-clear-icon" (click)="clearTextInput()" (mousedown)="clearTextInput()"></button>' +
 	                '<div (touchstart)="pointerStart($event)" (touchend)="pointerEnd($event)" (mousedown)="pointerStart($event)" (mouseup)="pointerEnd($event)" class="input-cover" tappable *ngIf="_useAssist"></div>',
 	            directives: [
 	                native_input_1.NextInput,
@@ -65825,13 +65828,6 @@
 	            this.setFocus();
 	            this.regScrollMove();
 	        }
-	    };
-	    /**
-	      * @private
-	     */
-	    InputBase.prototype.clearTextInput = function () {
-	        console.debug('Should clear input');
-	        this._value = '';
 	    };
 	    /**
 	     * @private

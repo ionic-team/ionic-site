9f197f9fef046050cda998c1db1edff7723d267e
Ionitron <hi@ionicframework.com>
2016-05-17 12:52:24 +0000

    Automated build of ionic  v2.0.0-beta.6 driftyco/ionic@8b09733124dc0389cf9903b33f349967ffe96091

--- docs/v2/demos/refresher/bundle.js
+++ docs/v2/demos/refresher/bundle.js
@@ -35311,23 +35311,21 @@
 	    });
 	    var words = template.split(' ').filter(function (w) { return w.length > 0; });
 	    words.forEach(function (word, i) {
-	        if (word.length) {
-	            FORMAT_KEYS.forEach(function (format) {
-	                if (word === format.f) {
-	                    if (word === FORMAT_A || word === FORMAT_a) {
-	                        // this format is an am/pm format, so it's an "a" or "A"
-	                        if ((formats.indexOf(FORMAT_h) < 0 && formats.indexOf(FORMAT_hh) < 0) ||
-	                            (words[i - 1] !== FORMAT_m && words[i - 1] !== FORMAT_mm)) {
-	                            // template does not already have a 12-hour format
-	                            // or this am/pm format doesn't have a minute format immediately before it
-	                            // so do not treat this word "a" or "A" as an am/pm format
-	                            return;
-	                        }
+	        FORMAT_KEYS.forEach(function (format) {
+	            if (word === format.f) {
+	                if (word === FORMAT_A || word === FORMAT_a) {
+	                    // this format is an am/pm format, so it's an "a" or "A"
+	                    if ((formats.indexOf(FORMAT_h) < 0 && formats.indexOf(FORMAT_hh) < 0) ||
+	                        (words[i - 1] !== FORMAT_m && words[i - 1] !== FORMAT_mm)) {
+	                        // template does not already have a 12-hour format
+	                        // or this am/pm format doesn't have a minute format immediately before it
+	                        // so do not treat this word "a" or "A" as an am/pm format
+	                        return;
 	                    }
-	                    formats.push(word);
 	                }
-	            });
-	        }
+	                formats.push(word);
+	            }
+	        });
 	    });
 	    return formats;
 	}

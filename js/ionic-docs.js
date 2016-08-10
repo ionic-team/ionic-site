c109cefc555184f705742c70626e906ea856342d
perry <perry@ionic.io>
2016-08-10 10:36:20 -0500

    adding the old analytics back in

--- js/ionic-docs.js
+++ js/ionic-docs.js
@@ -20,14 +20,20 @@ var _sf_async_config = { uid: 54141, domain: 'ionicframework.com', useCanonical:
 
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-44023830-23', 'auto', {'allowLinker': true});
-  ga('require', 'linker');
-  ga('linker:autoLink', ['ionic.io','blog.ionic.io','apps.ionic.io',
-     'creator.ionic.io','ideas.ionic.io','showcase.ionic.io','market.ionic.io']);
-  ga('send', 'pageview');
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-44023830-1', 'ionicframework.com');
+
+ga('create', 'UA-77242104-1', 'auto');
+
+// Universal ID
+ga('create', 'UA-44023830-23', 'auto', {'allowLinker': true}, 'universalID');
+ga('require', 'linker');
+ga('linker:autoLink', ['ionic.io','blog.ionic.io','apps.ionic.io',
+   'creator.ionic.io','ideas.ionic.io','showcase.ionic.io','market.ionic.io']);
+ga('send', 'pageview');
 
 var IonicDocsModule = angular.module('IonicDocs', ['ngAnimate'])
 .controller('DocsNavCtrl', ['$scope', '$timeout', function($scope, $timeout) {
@@ -1740,6 +1746,12 @@ document.getElementById('sidebar-toggle').onclick = function() {
 
 $(document).ready(function() {
   $('.dropdown-toggle').dropdown();
+  $('#collapse-toggle').unbind().click(function() {
+    $('#navbar-collapse').collapse('toggle');
+    var ariaVal = $('#collapse-toggle').attr('aria-expanded');
+    ariaVal = ariaVal === 'false' ? 'true' : 'false';
+    $('#collapse-toggle').attr('aria-expanded', ariaVal);
+  });
 });
 
 // highlight.js syntax highlighting

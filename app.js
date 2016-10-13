59387955a5c3fc0dbb6863bd4467bd0af61f7a2d
Brian Dennis <briandennis321@gmail.com>
2016-10-13 15:37:56 -0500

    add request processing back in

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require("express"),
     app = express(),
     url = require('url'),
-    compress = require('compression');
+    compress = require('compression'),
+    processRequest = require('./server/processRequest');
 
 process.env.PWD = process.cwd()
 
@@ -25,34 +26,9 @@ app.use(function(req, res, next) {
   }
 });
 
-// don't index unless production
-app.use(function(req, res, next) {
-  if (req.hostname.indexOf('ionicframework.com') == -1) {
-    res.setHeader('X-Robots-Tag:', 'noindex, nofollow');
-  }
-  return next();
-});
-
-function wwwRedirect(req, res, next) {
-  if (req.headers.host.slice(0, 4) === 'www.') {
-    var newHost = req.headers.host.slice(4);
-    return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
-  }
-  next();
-};
 app.set('trust proxy', true);
-app.use(wwwRedirect);
 app.use(compress());
-app.use(function(req, res, next) {
-  var staticURLS = ['/img/','/css/','/js/','/fonts/','/favicon.ico'];
-  for (var i = 0; i < staticURLS.length; i++) {
-    if (req.url.indexOf(staticURLS[i]) === 0) {
-      res.setHeader('Cache-Control', 'public, max-age=345600'); // 4 days
-      res.setHeader('Expires', new Date(Date.now() + 345600000).toUTCString());
-    }
-  }
-  return next();
-});
+app.use(processRequest);
 
 app.use(express.static(process.env.PWD));
 

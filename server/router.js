fa15fc9850636b1cbd8140485eab8e82b8affd5e
perry <perry@ionic.io>
2017-02-02 11:31:57 -0600

    re-enabling pricing test

--- server/router.js
+++ server/router.js
@@ -3,14 +3,14 @@ var ab         = require('express-ab');
 
 module.exports = function router(app) {
 
-  var abTestTest = ab.test('pricing-test', { id: 'u8whhVtTT3G5S3S7MZkoOA' });
+  var abTestTest = ab.test('pricing-test', {id: 'u8whhVtTT3G5S3S7MZkoOA'});
 
   return new express.Router()
-  // .get('/css/styles.min.css', abTestTest(null, 0.99), function(req, res) {
-  //   res.sendFile('/css/styles.min.css',{root:'./_site'});
-  // })
-  // .get('/css/styles.min.css', abTestTest(null, 0.01), function(req, res) {
-  //   res.sendFile('/css/styles.test.min.css',{root:'./_site'});
-  // });
+  .get('/css/styles.min.css', abTestTest(null, 0.95), function(req, res) {
+    res.sendFile('/css/styles.min.css', {root: './_site'});
+  })
+  .get('/css/styles.min.css', abTestTest(null, 0.05), function(req, res) {
+    res.sendFile('/css/styles.test.min.css', {root: './_site'});
+  });
 
 };

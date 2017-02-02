48b8bf7d4e29fe0fadfe4cef4fadb65374fd7159
perry <perry@ionic.io>
2017-02-02 11:51:25 -0600

    updating experiments key for the new pricing test

--- server/router.js
+++ server/router.js
@@ -3,7 +3,7 @@ var ab         = require('express-ab');
 
 module.exports = function router(app) {
 
-  var abTestTest = ab.test('pricing-test', {id: 'u8whhVtTT3G5S3S7MZkoOA'});
+  var abTestTest = ab.test('pricing-test', {id: 'MRClmJX_QEakg3kJs2jH9g'});
 
   return new express.Router()
   .get('/css/styles.min.css', abTestTest(null, 0.95), function(req, res) {

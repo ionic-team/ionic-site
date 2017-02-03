ec85a544a21a5ede5e1efd911ce61de203833899
Max Lynch <max@drifty.com>
2017-02-03 10:54:35 -0600

    Contact

--- assets/js/app.js
+++ assets/js/app.js
@@ -323,6 +323,27 @@ var IonicSiteModule = angular.module('IonicSite', ['ngAnimate', 'ngSanitize', 'i
       $scope.showSurvey = true;
     });
   };
+  $scope.submitContact = function() {
+    $scope.submitting = true;
+    $http.post('http://survey.apis.ionicjs.com/leadgen/v1pricing', {
+      email: $scope.form.email,
+      name: $scope.form.name,
+      phone: $scope.form.phone,
+      company: $scope.form.company,
+      role: $scope.form.role
+    }).then(function(res) {
+      $scope.thanks = true;
+      $scope.submitting = false;
+      $scope.showSurvey = true;
+    }, function(err) {
+      $scope.submitting = false;
+      var msg = 'Unable to reserve spot. Please contact help@ionic.io (see ' +
+        'console for more info)';
+      alert(msg);
+      console.error(err);
+      $scope.showSurvey = true;
+    });
+  };
 
   $scope.surveyQuestions = {
     questions: [

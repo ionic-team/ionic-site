a608337126adb73f7d890700a7f7d95a385096e2
Matthew Kremer <mkremer90@gmail.com>
2017-03-20 11:53:28 -0500

    Starting to work on contact page, added view pricing button

--- server/router.js
+++ server/router.js
@@ -16,5 +16,6 @@ module.exports = function router(app) {
   .get('/', (req, res) => { res.render('index'); })
   .get('/products', (req, res) => { res.render('products/index'); })
   .get('/products/pricing', (req, res) => { res.render('products/pricing'); })
+  .get('/products/contact', (req, res) => { res.render('products/contact'); })
   .get('/products/view', (req, res) => { res.render('products/view'); });;
 };

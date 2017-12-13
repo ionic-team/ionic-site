ddc9dd32125d16286dad8f6c8b441c7fbf90e5b6
perry <perry@ionic.io>
2017-12-13 17:17:10 -0600

    implementing google tag manager

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,44 +1,9 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-44023830-1', 'ionicframework.com');
-
-// Google Optimize
-ga('require', 'GTM-WKQJ324');
-
-ga('create', 'UA-77242104-1', 'auto');
-
-if (window.experiment) {
-  ga('set', 'expId', window.experiment.id);
-  ga('set', 'expVar', window.experiment.variant);
-}
-
-ga('require', 'Clearbit', {"mapping":{
-  "companyName":"dimension1",
-  "companyDomain":"dimension2",
-  "companyTags":"dimension3",
-  "companySector":"dimension4",
-  "companyIndustryGroup":"dimension5",
-  "companyIndustry":"dimension6",
-  "companySubIndustry":"dimension7",
-  "companyType":"dimension8",
-  "companyEmployeesRange":"dimension9",
-  "companyAlexaRank":"dimension10",
-  "companyCity":"dimension11",
-  "companyState":"dimension12",
-  "companyCountry":"dimension13",
-  "companyFunding":"dimension14",
-  "companyTech":"dimension15"
-}});
-
-// Universal ID
-ga('create', 'UA-44023830-23', 'auto', {'allowLinker': true}, 'universalID');
-ga('require', 'linker');
-ga('linker:autoLink', ['ionic.io','blog.ionic.io','apps.ionic.io',
-   'creator.ionic.io','ideas.ionic.io','showcase.ionic.io','market.ionic.io']);
-ga('send', 'pageview');
+// Google Tag Manager
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TKMGCBC');
 
 
 // shorthand global analytics click event helper

debb690b5333074f7b757f8636d5ea377c75b3d9
perry <perry@ionic.io>
2018-01-08 14:22:47 -0600

    fixing CSP rules

--- server/processRequest.js
+++ server/processRequest.js
@@ -57,7 +57,7 @@ module.exports = function(req, res, next) {
     res.setHeader('X-Robots-Tag', 'noindex, nofollow');
     protocol = 'http';
   } else if (req.protocol === 'http') {
-    res.setHeader('Content-Security-Policy', 'default-src https:');
+    res.setHeader('Content-Security-Policy', 'default-src https: \'unsafe-inline\'');
     // force https in prod
     // return res.redirect(301, `https://ionicframework.com${req.url}`);
   }

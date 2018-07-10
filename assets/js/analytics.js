598dca763e9b020e44aeec47feac31019b0c8394
Kevin Ports <kevin.ports@gmail.com>
2018-07-10 16:48:47 -0500

    1 ionic flow (#1393)
    * Update homepage styles
    
    * More homepage style updates
    
    * Rename homepage enterprise logos file
    
    * Update homepage styles
    
    * Add getting started tabs
    
    * Homepage updates
    
    * Add getting started form
    
    * Responsive styles for 1 ionic flow
    
    * Add logos to getting started sign up tab
    
    * esponsive styles and browser testing for 1 ionic
    
    * Add 1 Ionic tracking codes
    
    * Resurrect old home and getting started pages for a/b tests
    
    * Implement sign up form, add hooks for a/b tests
    
    * Change ajax post to use vanilla XMLHttpRequest api to fix jQuery Safari issue
    
    * Remove a/b test page structure. Rename 1 Ionic files and keep old templates for backup. Better getting-started page tab deep linking
    
    * Point getting started form submission to production dashboard & api

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -29,7 +29,22 @@ $(function() {
     // console.log(els)
     els.forEach(function(el) {
       var href = el.getAttribute('href');
+
+      // https://stackoverflow.com/questions/44412241/is-it-possible-to-read-experimentid-and-variationid-in-javascript-with-google-op
+      // var propertyId = "UA-44023830-1";
+      // var experimentId = Object.keys(gaData[propertyId].experiments)[0];
+      // var variationId = gaData[propertyId].experiments[experimentId];
+      // console.log(experimentId, variationId)
+
       if (href.indexOf('?') !== -1) {
+
+        // If a source attribute has been added to URI and an A/B test is running, append test group ID to value of source attribute
+        // var sourceVal = getQuerystringParameter('source', '?' + href.split('?')[1]);
+        // if (sourceVal.length && experimentId.length) {
+        //   href = updateQuerystringParameter(href, 'source', sourceVal + '-' + experimentId + '-' + variationId);
+        // }
+        // console.log(href)
+
         href += '&'
       } else {
         href += '?'
@@ -46,7 +61,23 @@ $(function() {
   linker();
 });
 
+function getQuerystringParameter(name, querystring) {
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+  var results = regex.exec(querystring);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
 
+function updateQuerystringParameter(uri, key, value) {
+  var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+  var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+  if (uri.match(re)) {
+    return uri.replace(re, '$1' + key + "=" + value + '$2');
+  }
+  else {
+    return uri + separator + key + "=" + value;
+  }
+}
 
 // shorthand global analytics click event helper
 window.c = function(cat, lbl, el, val) {

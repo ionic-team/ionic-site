aee1ce7b661a8460aef15cbb8c65aec69ec0819b
perry <perry@ionic.io>
2018-08-06 16:07:27 -0500

    what is ionic

--- stencil.config.js
+++ stencil.config.js
@@ -6,7 +6,7 @@ exports.config = {
     sass()
   ],
   copy: [
-    // { src: 'node_modules/ionicons/dist/collection/icon/svg/md-search.svg' }
+    { src: '../../node_modules/ionicons/dist/collection/icon/svg' }
   ],
   outputTargets: [
     // {

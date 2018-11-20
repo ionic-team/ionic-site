27e32019897d2ea4bed30d9e4709da90f1f7da31
Kevin Ports <kevin.ports@gmail.com>
2018-11-19 21:18:05 -0600

    Pricing page updates wip

--- server/router.js
+++ server/router.js
@@ -82,7 +82,7 @@ module.exports = function router(app) {
 
   .get('/jobs', (_, res) => res.render('jobs'))
   .get('/press', (_, res) => res.render('press'))
-  .get('/pro/pricing', (_, res) => res.render('pro/pricing'))
+  .get('/pro/pricing', (_, res) => res.render('pricing/index'))
   // .get('/pro/pricing/table', (_, res) => { res.render('pro/pricing/table'); })
   // .get('/pricing/trial', (_, res) => { res.render('pricing/trial'); })
   .get('/privacy', (_, res) => markdown(res, 'privacy-policy'))

278da9d0db01fc0bf8c9ac983d4211b4abe0ac48
perry <perry@ionic.io>
2019-04-09 11:17:58 -0500

    appflow widget tweak

--- assets/stencil/components/ionic-section-activatior/ionic-section-activatior.tsx
+++ assets/stencil/components/ionic-section-activatior/ionic-section-activatior.tsx
@@ -15,61 +15,141 @@ import {
   shadow: false
 })
 export class IonicSectionActivator {
-  @State() $currentItem: HTMLElement;
+  @State() $circles = [];
+  @State() $lis = [];
   @Element() el;
 
-  duration = 10
-  r = 58;
-  scrollRef: null;
+  duration = 6//seconds
+  quickDuration = .25 //seconds
+  r = 31; // radius
+  circumference: number;
+  gsRefs = [];
+
+  constructor()  {
+    this.circumference = this.r * 2 * Math.PI;
+    this.animationSelect = this.animationSelect.bind(this);
+    this.animationStart = this.animationStart.bind(this);
+  }
+
+  componentDidLoad() {
+    setTimeout(this.animationStart, 2000, 0);
+    // setTimeout(this.animationStart, 3000, 2);
+    // setTimeout(this.animationStart, 4000, 3);
+
+    // recursive function to add all the circles
+    const addCircle = (li, i) => {
+      this.$lis[i] = li;
+      this.$circles[i] = li.querySelector('.progress-ring__circle');
+      if (li.nextElementSibling && li.nextElementSibling.nodeName === 'LI') {
+        addCircle(li.nextElementSibling, i + 1);
+      }
+    }
+
+    addCircle(this.el.querySelector('li:nth-child(1)'), 0);
+  }
 
   animationStart(index) {
-    this.$currentItem = this.el.querySelector(`li:nth-child(${index}) circle`);
+    this.$lis[index].classList.add('active');
 
-    this.scrollRef = TweenLite.to(this.$currentItem , this.duration, {
+    TweenLite.to(this.$circles[index], .4, {
+      opacity: 1
+    });
+
+    TweenLite.to(this.$circles[index], this.duration, {
       strokeDashoffset: 0,
       onCompleteScope: this,
-      onComplete: function() {
-        console.log('gets here')
-        // jump back to starting position and restart the scroll in 1 frame
-        // TweenLite.to(this.$countries, 0, {
-        //   x: 0,
-        //   lazy:true,
-        //   onCompleteScope: this,
-        //   onComplete: function() {
-        //     this.scroll();
-        //   }
-        // })
+      onComplete: () =>  {
+        // fade out and start the next animation
+        this.animationStart(
+          index >= this.$circles.length - 1 ? 0 : index + 1
+        );
+        this.$lis[index].classList.remove('active');
+        TweenLite.to(this.$circles[index], .2, {
+          opacity: 0,
+          onCompleteScope: this,
+          onComplete: () => {
+            // jump back to starting position
+            TweenLite.to(this.$circles[index], 0, {
+              strokeDashoffset: this.circumference,
+              lazy:true
+            });
+          }
+        });
       }
     })
   }
 
-  componentDidLoad() {
-    setTimeout(this.animationStart.bind(this), 2000, 1);
-    setTimeout(this.animationStart.bind(this), 3000, 2);
-    setTimeout(this.animationStart.bind(this), 4000, 3);
+  animationSelect(index) {
+    this.$lis[index].classList.add('active');
+
+    this.animationStopOthers(index)
+    TweenLite.to(this.$circles[index], this.quickDuration, {
+      strokeDashoffset: 0,
+      opacity: 1,
+      onCompleteScope: this,
+      onComplete: function() {
+        // this.animationStart(index);
+      }}
+    )
+  }
+
+  animationRestart(index) {
+    TweenLite.to(this.$circles[index], .8, {
+      strokeDashoffset: this.circumference * -1,
+      lazy: true,
+      onCompleteScope: this,
+      onComplete: function() {
+        TweenLite.to(this.$circles[index], 0, {
+          strokeDashoffset: this.circumference,
+          opacity: 0,
+          lazy: true,
+          onCompleteScope: this,
+          onComplete: function() {
+            this.animationStart(index);
+          }
+        })
+      }
+    })
+  }
+
+  animationStopOthers(index) {
+    const circles = [];
+    this.$circles.forEach((circle, i) => {
+      if (i != index ) {
+        circles.push(circle);
+        this.$lis[i].classList.remove('active');
+      }
+    });
+    TweenLite.to(circles, .2, {
+      opacity: 0,
+      lazy: true,
+      onCompleteScope: this,
+      onComplete: function() {
+        TweenLite.to(circles, 0, {
+          strokeDashoffset: this.circumference,
+          opacity: 0,
+          lazy: true
+        });
+      }
+    })
   }
 
   circle(percent = 0) {
-    const circumference = this.r * 2 * Math.PI;
     return (
-      <svg
-        class="progress-ring"
-        height="120"
-        width="120">
-        <circle
-          class="progress-ring__circle"
-          stroke="white"
-          stroke-width="4"
-          fill="transparent"
-          r={this.r}
-          cx="60"
-          cy="60"
-          style={{
-            strokeDasharray: `${circumference} ${circumference}`,
-            strokeDashoffset: circumference - percent / 100 * circumference
-
-          }}
-        />
+      <svg class="progress-ring" height="64" width="64" >
+        <circle class="progress-ring__circle"
+                stroke="#6C89F7"
+                stroke-width="2"
+                fill="transparent"
+                r={this.r}
+                cx="32"
+                cy="32"
+                style={{
+                  strokeDasharray: 
+                    `${this.circumference} ${this.circumference}`,
+                  strokeDashoffset: 
+                    this.circumference - percent / 100 * this.circumference
+                }}/>
       </svg>
     )
   }
@@ -78,15 +158,18 @@ export class IonicSectionActivator {
     return (
       <nav>
         <ul>
-          <li>
-            {this.circle(0)}
+          <li onMouseEnter={() => this.animationSelect(0)}
+              onMouseLeave={() => this.animationRestart(0)}>
+            {this.circle()}
             <slot name="1"></slot>
           </li>
-          <li>
-            {this.circle(0)}
+          <li onMouseEnter={() => this.animationSelect(1)}
+              onMouseLeave={() => this.animationRestart(1)}>
+            {this.circle()}
             <slot name="2"></slot>
           </li>
-          <li>
+          <li onMouseEnter={() => this.animationSelect(2)}
+              onMouseLeave={() => this.animationRestart(2)}>
             {this.circle()}
             <slot name="3"></slot>
           </li>

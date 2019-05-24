bf00ce78faa3ae13400e269b8e7acc015ddf0389
perry <perry@ionic.io>
2019-05-24 08:56:27 -0500

    CSP rules for API endpoints

--- server/controllers/newsletterCtrl.js
+++ server/controllers/newsletterCtrl.js
@@ -28,6 +28,12 @@ module.exports = function(req, res) {
   // relfect because we want to show the page even if one of the tasks error
   Promise.all(promises.map(reflect)).then(values => {
     res.header('Access-Control-Allow-Origin', '*');
+
+    let csp = 'upgrade-insecure-requests; default-src https:';
+    res.header('Content-Security-Policy', csp);
+    res.header('X-Content-Security-Policy', csp);
+    res.header('X-WebKit-CSP', csp);
+
     res.json({ ok: true, message: `${email} added to newsletter${podcast ? 
       ' and podcast list' : ''}` });
   });

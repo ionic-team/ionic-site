98912e621ace0b7622646f1e1e407a39c1e31d1a
perry <perry@ionic.io>
2019-07-29 17:07:07 -0500

    refactoring the sub-header as a stencil component

--- assets/stencil/components/ionic-sub-header/ionic-sub-header.tsx
+++ assets/stencil/components/ionic-sub-header/ionic-sub-header.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, State, h } from '@stencil/core';
+import { Component, Element, Prop, State, h } from '@stencil/core';
 
 @Component({
   tag: 'ionic-sub-header',
@@ -10,9 +10,63 @@ export class IonicSubHeader {
   @Prop() darkMode = true;
 
   @State() dropdownActive = false;
+  @State() activeClass = '';
+  @State() stuck = false;
+  @State() queued = false;
+  @Element() el;
+
+  observer: IntersectionObserver;
 
   constructor() {
-    console.log(document.body.classList)
+    
+  }
+
+  getTriggerEl() {
+    // return (<div class="ionic-sub-header__trigger"></div>)
+    const trigger = document.createElement('div');
+    trigger.id = 'ionic-sub-header__trigger';
+    return trigger;
+  }
+
+  componentDidLoad() {
+    this.el.before(this.getTriggerEl());
+    this.init();
+    requestAnimationFrame(()=>{
+      this.el.classList.add('ionic-sub-header__initialized');
+    })
+  }
+
+  init() {
+    this.observer = new IntersectionObserver(entries => {
+      if (this.queued) return;
+  
+      // no intersection with screen
+    	if(!this.stuck && entries[0].intersectionRatio === 0) {
+        this.queued = true;
+        requestAnimationFrame(() => {
+          this.el.classList.add('ionic-sub-header--stuck');
+          this.stuck = true;
+          this.queued = false;
+        });
+  
+    	// fully intersects with screen
+      } else if(this.stuck && entries[0].intersectionRatio === 1) {
+        this.queued = true;
+        requestAnimationFrame(() => {
+          this.el.classList.remove('ionic-sub-header--stuck');
+          this.stuck = false;
+          this.queued = false;
+        })
+      }
+    }, { threshold: [0,1] })
+
+    if (this.el) {
+      this.observer.observe(document.getElementById('ionic-sub-header__trigger'));
+      setTimeout(() => {
+        this.el.classList.add('ionic-sub-header_initialized');
+        document.querySelector('.navbar-default').classList.add('navbar--not-fixed');
+      }, 405)
+    }
   }
 
   handleDropdownClick() {
@@ -21,7 +75,6 @@ export class IonicSubHeader {
 
   render() {
     return (
-      <div id="sub-header">
         <div class="container">
           <slot name="title"/>
           <slot name="breadcrumbs"/>
@@ -29,7 +82,6 @@ export class IonicSubHeader {
           <slot name="right"/>
           <slot name="button"/>
         </div>
-      </div>
     );
   }
 }

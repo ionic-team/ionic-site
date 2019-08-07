dc6773fc27116388ea7c891ac6e33bbe2c5e0f97
perry <perry@ionic.io>
2019-08-07 11:40:05 -0500

    v4.7.2

--- assets/stencil/components.d.ts
+++ assets/stencil/components.d.ts
@@ -58,11 +58,6 @@ export namespace Components {
 
 declare global {
 
-  // Adding a global JSX for backcompatibility with legacy dependencies
-  export namespace JSX {
-    export interface Element {}
-  }
-
 
   interface HTMLIconExternalElement extends Components.IconExternal, HTMLStencilElement {}
   var HTMLIconExternalElement: {

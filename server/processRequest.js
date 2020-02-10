ae7a09be64662120fe204a9c8e67da17ea16d66d
Lars Mikkelsen <lars@ionic.io>
2020-02-10 10:36:42 -0500

    Add login via single sign-on

--- server/processRequest.js
+++ server/processRequest.js
@@ -1,3 +1,4 @@
+var config          = require('./config');
 var employees       = require('./data/employees');
 var resources       = require('./data/resources');
 var frameworkInfo   = require('./data/framework-info');
@@ -60,6 +61,8 @@ module.exports = {
   loadLocalVars: async (req, res, next) => {
     // Setting default Page Vars
     res.locals = Object.assign({}, res.locals, {
+      API_URL: config.API_URL,
+      DASHBOARD_URL: config.DASHBOARD_URL,
       header_style: 'transparent',
       id: req.originalUrl.split('/').join('-'),
       employees: shuffle(employees),

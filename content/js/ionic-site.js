c22b58f06db5ddbbdf62e41292c6f2c6b2f26562
jaredcbaum <jaredcbaum@gmail.com>
2020-05-07 20:00:11 -0500

    trying defer again

--- content/js/ionic-site.js
+++ content/js/ionic-site.js
@@ -27,7 +27,7 @@ d.trigger("activate.bs.scrollspy")},b.prototype.clear=function(){a(this.selector
 ;// Google Tag Manager
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.defer=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-TKMGCBC');
 

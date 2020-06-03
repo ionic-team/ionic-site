d04078629c4df06e492500aae1118349cbb1fb24
jaredcbaum <jaredcbaum@gmail.com>
2020-06-02 22:20:21 -0500

    changed to ioniconf url

--- server/router.js
+++ server/router.js
@@ -104,7 +104,6 @@ module.exports = {
       getIntegration(req, res, next, req.params.integration))
 
     .get('/ioniconf', (_, res) => res.render('ioniconf'))
-    .get('/ioniconffull', (_, res) => res.render('ioniconffull'))
     .get('/jobs', (_, res) => res.render('jobs'))
     .get('/login', (_, res) => res.render('login'))
     .get('/logout', (_, res) => res.render('logout'))

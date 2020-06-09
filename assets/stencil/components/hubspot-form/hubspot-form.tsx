392430b137b3366eecb44f42b409874a431860ab
Jared B <jaredcbaum@gmail.com>
2020-06-09 16:33:15 -0500

    init commit

--- assets/stencil/components/hubspot-form/hubspot-form.tsx
+++ assets/stencil/components/hubspot-form/hubspot-form.tsx
@@ -1,4 +1,5 @@
-import { Component, Prop, h } from '@stencil/core';
+import { Component, Prop, Element, h, State } from '@stencil/core';
+
 
 @Component({
   tag: 'hubspot-form',
@@ -6,18 +7,45 @@ import { Component, Prop, h } from '@stencil/core';
   shadow: false
 })
 export class HubspotForm {
-
   @Prop() formId: string = 'default';
+  @Element() el: HTMLElement;
+  @State() blocked: boolean;
+  private formRows: Array<Array<Object>> = [];
 
   componentWillLoad() {
-    fetch(`/api/v1/getform/${this.formId}`)
-      .then(response => response.json())
-      .then(data => console.log(data));
+    const script = document.createElement('script');
+    script.onload = () => {
+      window['hbspt'].forms.create({
+        portalId: '3776657',
+        formId: this.formId,
+        target: 'hubspot-form'
+      });
+      ;
+    };
+    script.onerror = this.loadBackupForm;
+    script.src = '//js.hsforms.net/forms/v2.js';
+
+    this.el.appendChild(script);
+  }
+
+  loadBackupForm = async () => {
+    const response = await fetch(`/api/v1/getform/${this.formId}`)
+    const data = await response.json()
+
+    this.formRows = data.formFieldGroups.map(row => row.fields );
+    console.log(this.formRows);
+    this.blocked = true;
   }
 
   render() {
+    console.log('got here');
     return (
-      <h1>Hi</h1>
+      <div>
+        { this.blocked &&
+        <form>
+          {this.formRows.map(r => <HubspotFormRow props={r} />)}
+        </form> }
+      </div>
     );
   }
 }
